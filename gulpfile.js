41b8962ca3be0cc2882f8a5375723ecd3de5e7ec
Matthew <weeksling@gmail.com>
2016-04-10 13:39:19 -0400

    Added sass compilation to gulpfile, and added a demo.html page to play around with styles

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,24 @@ var react = require('gulp-react');
 var babel = require('gulp-babel')
 var htmlreplace = require('gulp-html-replace');
 var clean = require('gulp-clean');
+var sass  = require('gulp-ruby-sass');
+var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
 
-var path = {
-  HTML: 'src/client/index.html',
+var config = {
+  HTML: 'src/client/*.html',
   ALL: ['src/application/*.js', 'src/client/index.html'],
   JS: ['src/application/*.js', 'src/js/**/*.js'],
   MINIFIED_OUT: 'js/build.min.js',
+  STYLES_OUT: 'build/css',
   DEST: 'build'
 };
 
+
+gulp.task('default', ['replaceHTML', 'build', 'sass']);
+
 gulp.task('build', function(){
-  gulp.src(path.JS)
+  gulp.src(config.JS)
     .pipe(babel({
         only: [
           'src/application',
@@ -22,18 +29,33 @@ gulp.task('build', function(){
         presets: ['react'],
         compact: false
       }))
-    .pipe(concat(path.MINIFIED_OUT))
-    .pipe(gulp.dest(path.DEST));
+    .pipe(concat(config.MINIFIED_OUT))
+    .pipe(gulp.dest(config.DEST));
 });
 
 gulp.task('replaceHTML', function(){
-  gulp.src(path.HTML)
+  gulp.src(config.HTML)
     .pipe(htmlreplace({
-      'js': 'build/' + path.MINIFIED_OUT
+      'js': 'build/' + config.MINIFIED_OUT
     }))
-    .pipe(gulp.dest(path.DEST));
+    .pipe(gulp.dest(config.DEST));
 });
 
-gulp.task('default', ['replaceHTML', 'build']);
+/**
+ * Compile less to css
+ * @return {Stream}
+ */
+gulp.task('sass', ['clean-styles'], function() {
+    return sass(['src/client/css/style.scss'])
+        .on('error', sass.logError)
+        //.pipe(autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(livereload());
+});
 
 
+gulp.task('clean-styles', function () {
+  return gulp
+      .src(config.STYLES_OUT)
+      .pipe(clean())
+})
