7e63a75e2eb461f6cfda25abc51bfa905ff57788
Matthew <weeksling@gmail.com>
2016-04-11 05:50:02 -0400

    Renamed the routes to /elements and /templates:

--- src/server/api/api.js
+++ src/server/api/api.js
@@ -6,14 +6,14 @@ var router = express.Router()
 
 // more routes for our API will happen here
 // http://localhost:8080/api/template?id=4
-router.get('/template', function(req, res){
+router.get('/templates', function(req, res){
   // path to graphics
   var graphics = '[{"source":"/icons/alarms.svg"},{"source":"/icons/apartment.svg"},{"source":"/icons/arrow-down-circle.svg"}]';
   var arr = JSON.parse(graphics);
   res.json(arr);
 } );
 
-router.get('/graphics', function(req, res){
+router.get('/elements', function(req, res){
   // path to graphics
   var graphics = [{"id": 0212, "source":"/icons/alarm.svg"},{"id": 0213, "source":"/icons/apartment.svg"},{"id": 0214, "source":"/icons/arrow-down-circle.svg"}];
   //var arr = JSON.parse(graphics);
